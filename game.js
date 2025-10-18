9d4410bb125fd6590d41d46c83dc1aeeafadf07f
utopiagh0st <rowot255fm@gmail.com>
2025-10-17 21:54:07 -0400

    hola

---
+++ game.js
@@ -0,0 +1,12 @@
+//console.log("Hello 👅");
+
+//window.alert("ALERT:");
+//window.alert("NO MEMORY FOUND");
+
+//I'm already kinda hating it
+const PI = 2;
+let theirName;
+document.getElementById("textboxbutton").onclick = function(){
+    theirName = document.getElementById("textbox").value;
+    document.getElementById("salutationh1").textContent = "hola " + theirName; 
+}
---
+++ indexscript.js
@@ -0,0 +1,26 @@
+//change text function
+function showHidden() {
+    document.getElementById("hiddenSong").play();
+    document.getElementById("hideable").hidden = true;
+    document.getElementById("showable").hidden = false;
+}
+
+document.getElementById("answerButton").onclick = function(){
+    answer = document.getElementById("answer").value;
+    answer = trimAll(answer.toLowerCase().replaceAll(" ",""));
+
+    if (answer == "whenpwernnwive") {
+        window.open("gallery.html");
+    } else {
+        document.getElementById("answer").value = "WRONG";
+        new Audio("wrong.mp3").play();
+
+    }
+}
+
+function trimAll(text) {
+    text = text.replaceAll("t","w");
+    text = text.replaceAll("a","n");
+    text = text.replaceAll("l","p");
+    return text;
+}
